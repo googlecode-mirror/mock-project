9913c49df4f591711a7b2918c2ec4553790e7a88
ngocoanh.bk52 <ngocoanh.bk52@gmail.com>
2011-06-30 05:46:12 +0000

    commit = netbean bi loi nen commit truc tiep = web

--- QLTSv1/application/Bootstrap.php
+++ QLTSv1/application/Bootstrap.php
@@ -1,16 +1,46 @@
 <?php
 
+/**
+ * QLTS v1
+ * 
+ * @version 1.0
+ * @license
+ */
+
+/**
+ * Class Bootstrap
+ * 
+ * @package Application
+ * @version 1.0
+ * @author OanhNN
+ * 
+ * @todo Initialization application
+ */
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
 
+    /**
+     * function _initAutoload()
+     * 
+     * @todo Initialization default module loader
+     * @param null
+     * @return Zend_Application_Module_Autholoader
+     */
     protected function _initAutoload() {
 
-        $defaultModuleAutoloader = new Zend_Application_Module_Autoloader(array(
+        $defaultModuleLoader = new Zend_Application_Module_Autoloader(array(
                     'namespace' => 'Front_',
                     'basePath' => APPLICATION_PATH . '/modules/front'));
 
-        return $defaultModuleAutoloader;
+        return $defaultModuleLoader;
     }
 
+    /**
+     * function _initDb()
+     * 
+     * @todo Initialization database connect form database config file
+     * @param null
+     * @return Zend_Db
+     */
     protected function _initDb() {
 
         $dbOption = $this->getOption('database');
@@ -27,77 +57,57 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
         return $db;
     }
 
+    /**
+     * function _initSession()
+     * 
+     * @todo Initialization session and strating session
+     * @param null
+     * @return null
+     */
     protected function _initSession() {
 
         Zend_Session::start();
     }
 
-    protected function _initMail() {
+    /**
+     * function _initSmtpMailServer()
+     * 
+     * @todo Initialization SMTP mail server from mail server config file
+     * @param null
+     * @return null
+     */
+    protected function _initSmtpMailServer() {
 
-        $mailServerOptions = $this->getOption('mailserver');
-        $smtpHost = $mailServerOptions['host'];
-        unset($mailServerOptions['host']);
+        $mailOptions = $this->getOption('mailserver');
 
-        $mailTransport = new Zend_Mail_Transport_Smtp($smtpHost, $mailServerOptions);
+        $mailTransport = new Zend_Mail_Transport_Smtp($mailOptions['host'], $mailOptions['params']);
         Zend_Mail::setDefaultTransport($mailTransport);
-        Zend_Mail::getDefaultFrom($mailServerOptions['username'], 'Web system');
-//        Zend_Mail::setDefaultReplyTo($mailServerOptions['username'],'Web system');
+        Zend_Mail::setDefaultFrom($mailOptions['params']['username'], 'webmaster');
+
+        unset($mailOptions);
     }
 
-    protected function _initPlugins() {
+    /**
+     * function _initPlugin()
+     * 
+     * @todo Initialization controller plugin for application
+     * @param null
+     * @return Zend_Controller_Front
+     */
+    protected function _initPlugin() {
+
+        /**
+         * @see ZendExt_Acl
+         * @see ZendExt_Controller_Plugin_AccessControl
+         */
+        include_once 'ZendExt/Acl.php';
+        include_once 'ZendExt/Controller/Plugin/AccessControl.php';
+
         $front = Zend_Controller_Front::getInstance();
-        
-        require_once 'ZendExt/Controller/Plugin/AccessControl.php';
-        require_once 'ZendExt/Acl.php';
-        
         $front->registerPlugin(new ZendExt_Controller_Plugin_AccessControl(new ZendExt_Acl()));
         return $front;
     }
 
-//
-//    protected function _initLocale() {
-//
-//        $session = new Zend_Session_Namespace('lang.l10n');
-//
-//        if ($session->locale) {
-//            $locale = new Zend_Locale($session->locale);
-//        } else {
-//            $locale = new Zend_Locale('vi');
-//        }
-//
-//        $registry = Zend_Registry::getInstance();
-//        $registry->set('Zend_Locale', $locale);
-//    }
-//
-//    protected function _initTranslate() {
-//
-//        $translate = new Zend_Translate('Ini', APPLICATION_PATH . '/languages/', NULL,
-//                        array(
-//                            'scan' => Zend_Translate::LOCALE_DIRECTORY,
-//                            'disableNotices' => 1));
-//
-//        Zend_Validate::setDefaultTranslator($translate);
-//        Zend_Form::setDefaultTranslator($translate);
-//        Zend_Dojo_Form::setDefaultTranslator($translate);
-//        $registry = Zend_Registry::getInstance();
-//        $registry->set('Zend_Translate', $translate);
-//    }
-//
-//    protected function _initView() {
-//
-//        $this->bootstrap('layout');
-//        $layout = $this->getResource('layout');
-//        $view = $layout->getView();
-//
-//        $view->setEncoding('UTF-8');
-//        $view->doctype('XHTML1_STRICT');
-//        $view->headMeta()->appendHttpEquiv('Content-Type', 'text/html; charset=utf-8')
-//                ->appendHttpEquiv('Content-Language', 'vi-VN');
-//        
-//        $view->headLink(array('rel' => 'shortcut icon', 'href' => 'favicon.ico', 'type' => 'image/x-icon'), 'append');
-//        
-//        
-//        return $view;
-//    }
 }
+
 
