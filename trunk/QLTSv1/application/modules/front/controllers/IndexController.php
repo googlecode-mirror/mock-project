996fa261cfd72375472ec74f2fc40cb40dab3cca
ngocoanh.bk52@gmail.com <ngocoanh.bk52@gmail.com@df0fb54e-ee91-436e-b052-fdc5a6ca5d22>
2011-07-09 02:30:34 +0000

    minh phai ve que, co gi minh se update ngay

--- trunk/QLTSv1/application/modules/front/controllers/IndexController.php
+++ trunk/QLTSv1/application/modules/front/controllers/IndexController.php
@@ -23,29 +23,6 @@ class Front_IndexController extends Zend_Controller_Action {
         // action body
         echo "Hello world <br />";
 
-        include_once APPLICATION_PATH . '/modules/User/models/DbTable/Member.php';
-        $members = new User_Model_DbTable_Member();
-//        $data = new Zend_Db_Table('memberinfor');
-//        $members = $data->select()->from('memberinfor');
-        
-        $grid = new Ingot_JQuery_JqGrid('bookshelf',
-                        new Ingot_JQuery_JqGrid_Adapter_DbTableSelect($members->select()));
-        $col = new Ingot_JQuery_JqGrid_Column('UserID');
-        $col->setLabel('User Id');
-        $grid->addColumn($col);
-        $grid->addColumn(new Ingot_JQuery_JqGrid_Column('Username'));
-//        $grid->addColumn(new Ingot_JQuery_JqGrid_Column('Role'));
-        $grid->addColumn(new Ingot_JQuery_JqGrid_Column('FullName'));
-        $grid->addColumn(new Ingot_JQuery_JqGrid_Column('Email'));
-        $grid->addColumn(new Ingot_JQuery_JqGrid_Column('Group'));
-        $grid->addColumn(new Ingot_JQuery_JqGrid_Column('Birthday'));
-        $grid->addColumn(new Ingot_JQuery_JqGrid_Column('Phone'));
-        $grid->addColumn(new Ingot_JQuery_JqGrid_Column('Address'));
-
-//                 $footer = new Ingot_JQuery_JqGrid_Plugin_Pager();
-
-        $grid->registerPlugin(new Ingot_JQuery_JqGrid_Plugin_ToolbarFilter());
-        $this->view->grid = $grid->render();
     }
 
     public function aboutAction() {
