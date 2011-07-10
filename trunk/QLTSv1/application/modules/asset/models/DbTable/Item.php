38ba0bde6bf1ea8eec3c2e113913920203cad7ed
ngocoanh.bk52@gmail.com <ngocoanh.bk52@gmail.com@df0fb54e-ee91-436e-b052-fdc5a6ca5d22>
2011-07-10 18:46:27 +0000

    Sua Model Item

--- trunk/QLTSv1/application/modules/asset/models/DbTable/Item.php
+++ trunk/QLTSv1/application/modules/asset/models/DbTable/Item.php
@@ -1,200 +1,206 @@
 <?php
-/*;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;       Item
-;
-;       @package Modules/asset/models
-;       @version 1.0
-;       @author TuanNA18
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+/**
+ * QLTS v1
  * 
+ * @version 1.0
+ * @license
  */
-	 require_once 'Zend/Db/Table/Abstract.php'; 
-     class Asset_Model_DbTable_Item extends Zend_Db_Table_Abstract
-    {
-        protected $_name = 'iteminfor';     //Ten bang thao tac
 
-        //Them tai san moi voi cac thuoc tinh cua no
-        public function addItem($Ma,$Ten,$Description,$Type,$StartDate,$Price,$WarrantyTime,$Status,$Place){
-             $data = array(
-                    'Ma_tai_san'=>$Ma,
-                    'Ten_tai_san'=>$Ten,
-                    'Description'=>$Description,
-                    'Type'=>$Type,
-                    'StartDate'=>$StartDate,
-                    'Price'=>$Price,
-                    'WarrantyTime'=>$WarrantyTime,
-                    'Status'=>$Status,
-                    'Place'=>$Place
-                    );
-             if ($this->insert($data)!=true)
-             {
-                 return 0;                      //Thông tin nhập không hợp lệ
-             }
-             return 1;
+/**
+ * Class User_Model_DbTable_Member
+ * 
+ * @package Application/Module/User
+ * @version 1.0
+ * @author TuanNA18
+ * 
+ * @todo Model data from iteminfor table in database
+ */
+class Asset_Model_DbTable_Item extends Zend_Db_Table_Abstract {
+
+    /**
+     * @var $_name String
+     */
+    protected $_name = 'iteminfor';     //Ten bang thao tac
+
+    /**
+     * function addItem()
+     * 
+     * @todo add new item
+     * @param $MaTS             Int
+     * @param $TenTS            String
+     * @param $Description      String
+     * @param $Type             Enum(0,1)
+     * @param $StartDate        Date
+     * @param $Price            Int
+     * @param $WarrantyTime     Int
+     * @param $Status           Enum(0,1,2)
+     * @param $Place            String
+     * @return   1  success
+     * @return   0  error not insert
+     * @return  -1  error MaTS exist
+     */
+
+    public function addItem($MaTS, $TenTS, $Description, $Type, $StartDate, $Price, $WarrantyTime, $Status, $Place) {
+
+        if ($this->checkMaTS($MaTS) != NULL) {
+            return -1;
         }
 
-        //Lay thong tin tai san theo ID
-        public function getItemFromID($id){
-            $id=(int)$id;
-            $row = $this->fetchRow('ItemID = ' . $this->_db->quote($id, 'INTEGER'));
-            if (!$row) {
-               return null;
-            }
-            return $row->toArray();
+        $data = array(
+            'Ma_tai_san' => $MaTS,
+            'Ten_tai_san' => $TenTS,
+            'Description' => $Description,
+            'Type' => $Type,
+            'StartDate' => $StartDate,
+            'Price' => (int) $Price,
+            'WarrantyTime' => (int) $WarrantyTime,
+            'Status' => $Status,
+            'Place' => $Place
+        );
+        if ($this->insert($data)) {
+            return 1;
         }
-
-        //Xoa mot tai san theo ID cua no
-        public function deleteItem($id){
-             $id = (int) $id;
-             $arr=$this->getItemFromID($id);
-             if ($arr['Status']==1)
-             {
-                 return 0;                  //Lỗi item đang được cho mượn, không xóa được
-             }
-             $this->delete('id = ' .(int)$id);
-             return 1;
-        }
-
-        //Sua mot tai san voi cac thuoc tinh cua no
-        public function editItem($id,$Ma,$Ten,$Description,$Type,$StartDate,$Price,$WarrantyTime,$Status,$Price){
-             $data = array(
-                    'Ma_tai_san'=>$Ma,
-                    'Ten_tai_san'=>$Ten,
-                    'Description'=>$Description,
-                    'Type'=>Type,
-                    'Start Date'=>$StartDate,
-                    'Price'=>$Price,
-                    'WarrantyTime'=>$WarrantyTime,
-                    'Status'=>$Status,
-                    'Place'=>$Place
-                    );
-             if ($this->update($data,'id = ' .(int)$id)!=true)
-             {
-                 return 0;                   //Thông tin sửa không hợp lệ
-             }
-             return 1;
-        }
-
-        //Set thuoc tinh Ma theo ID
-        public function setMa($id,$value){
-             $data=array('Ma_tai_san'=>$value);
-             if ($this->insert($data)!=true)
-             {
-                 return 0;
-             }
-             return 1;
-        }
-
-        //Set thuoc tinh Ten theo id
-        public function setName($id,$value){
-             $data=array('Ten_tai_san'=>$value);
-             $this->update($data,'id = ' . $this->_db->quote($id, 'INTEGER'));
-        }
-
-        //Set thuoc tinh mo ta theo id
-        public function setDescription($id,$value){
-             $data=array('Description'=>$value);
-             $this->update($data,'id = ' . $this->_db->quote($id, 'INTEGER'));
-        }
-
-        //Set thuoc tinh kieu theo ID
-        public function setType($id,$value){
-             $data=array('Type'=>$value);
-             $this->update($data,'id = ' . $this->_db->quote($id, 'INTEGER'));
-        }
-
-        //Set thuoc tinh ngay bat dau mua theo ID
-        public function setStartDate($id,$value){
-             $data=array('Start Date'=>$value);
-             $this->update($data,'id = ' . $this->_db->quote($id, 'INTEGER'));
-        }
-
-        //Set thuoc tinh gia theo ID
-        public function setPrice($id,$value){
-             $data=array('Price'=>$value);
-             $this->update($data,'id = ' . $this->_db->quote($id, 'INTEGER'));
-        }
-
-        //Set thoi han bao hanh theo ID
-        public function setWarrantyTime($id,$value){
-             $data=array('WarrantyTime'=>$value);
-             $this->update($data,'id = ' . $this->_db->quote($id, 'INTEGER'));
-        }
-
-        //Set trang thai theo ID
-        public function setStatus($id,$value){
-             $data=array('Status'=>$value);
-             $this->update($data,'id = ' . $this->_db->quote($id, 'INTEGER'));
-        }
-
-        //Set vi tri theo ID
-        public function setPlace($id,$value){
-             $data=array('Place'=>$value);
-             $this->update($data,'id = ' . $this->_db->quote($id, 'INTEGER'));
-        }
-
-        
-
-        //Lay thong tin tai san theo Ma
-        public function getItemFromMa($Ma){
-             $row = $this->fetchRow("Ma_tai_san = '" . $this->_db->quote($Ma) . "'");
-             if (!$row) {
-               return null;
-            }
-            return $row->toArray();
-        }
-
-        //Lay thong tin tai san tu cac thong tin khac
-         public function getItemFromOthers($Ten, $Type, $Status, $Place, $StartPrice, $EndPrice, $StartTime, $EndTime,  $StartDate, $EndDate){
-            $sql = '1=1';
-            if ($Ten!=null)
-            {
-               $sql = $sql. ' AND Ten_tai_san LIKE %'.$Ten.'%';
-            }
-            if ($Type!=null)
-            {
-                $sql = $sql. ' AND Type = ' . $Type;
-            }
-            if ($Status!=null)
-            {
-                $sql = $sql. ' AND Status = ' . $Status;
-            }
-            if ($Place!=null)
-            {
-                $sql = $sql. ' AND Place LIKE %'.$Place.'%';
-            }
-            if ($StartPrice!=null)
-            {
-                $sql = $sql. ' AND Price >= ' . $this->_db->quote($StartPrice, 'INTEGER');
-            }
-            if ($EndPrice!=null)
-            {
-                $sql = $sql. ' AND Price <= ' . $this->_db->quote($EndPrice, 'INTEGER');
-            }
-            if ($StartTime!=null)
-            {
-                $sql = $sql. ' AND WarrantyTime >= ' . $this->_db->quote($StartTime, 'INTEGER');
-            }
-            if ($EndTime!=null)
-            {
-                $sql = $sql. ' AND WarrantyTime <= ' . $this->_db->quote($EndTime, 'INTEGER');
-            }
-            if ($StartDate!=null)
-            {
-                $sql = $sql. ' AND StartDate >= ' . $StartDate;
-            }
-            if ($EndDate!=null)
-            {
-                $sql = $sql. ' AND StartDate <= ' . $EndDate;
-            }
-            $sql=$sql.';';
-            $result = $this->fetchAll($this->select()->where($sql));
-            if (!$result)
-            {
-                return null;
-            }
-            return $result;
-        }
-
+        return 0;
     }
-?>
+
+    /**
+     * function getItemFromID()
+     * 
+     * @todo get member info
+     * @param $ItemID Int
+     * @return array() | NULL
+     */
+    public function getItemFromID($ItemID) {
+        $ItemID = (int) $ItemID;
+        $row = $this->fetchRow("ItemID = '$ItemID'");
+        if ($row == NULL) {
+            return NULL;
+        }
+        return $row->toArray();
+    }
+
+    /**
+     * function deleteItem()
+     * 
+     * @todo delete member
+     * @param $ItemID int
+     * @return boolen
+     */
+    public function deleteItem($ItemID) {
+        $ItemID = (int) $ItemID;
+        return $this->delete("ItemID = '$ItemID'");
+    }
+
+    /**
+     * function addItem()
+     * 
+     * @todo add new item
+     * @param $ItemID           Int
+     * @param $MaTS             Int
+     * @param $TenTS            String
+     * @param $Description      String
+     * @param $Type             Enum(0,1)
+     * @param $StartDate        Date
+     * @param $Price            Int
+     * @param $WarrantyTime     Int
+     * @param $Status           Enum(0,1,2)
+     * @param $Place            String
+     * @return   1  success
+     * @return   0  error not insert
+     * @return  -1  error MaTS exist
+     */
+    public function editItem($ItemID, $MaTS, $TenTS, $Description, $Type, $StartDate, $Price, $WarrantyTime, $Status, $Place) {
+        if ($ItemID == NULL) {
+            return 0;
+        }
+        $ItemID = (int) $ItemID;
+        if ($this->checkMaTS($MaTS) != $ItemID) {
+            return -1;
+        }
+        $data = array(
+            'Ma_tai_san' => $MaTS,
+            'Ten_tai_san' => $TenTS,
+            'Description' => $Description,
+            'Type' => $Type,
+            'StartDate' => $StartDate,
+            'Price' => (int) $Price,
+            'WarrantyTime' => (int) $WarrantyTime,
+            'Status' => $Status,
+            'Place' => $Place
+        );
+        if ($this->update($data, "ItemID = '$ItemID'")) {
+            return 1;
+        }
+        return 0;
+    }
+
+    //Lay thong tin tai san theo Ma
+    public function getItemFromMa($Ma) {
+        $row = $this->fetchRow("Ma_tai_san = '" . $this->_db->quote($Ma) . "'");
+        if (!$row) {
+            return null;
+        }
+        return $row->toArray();
+    }
+
+    //Lay thong tin tai san tu cac thong tin khac
+    public function getItemFromOthers($Ten, $Type, $Status, $Place, $StartPrice, $EndPrice, $StartTime, $EndTime, $StartDate, $EndDate) {
+        $sql = '1=1';
+        if ($Ten != null) {
+            $sql = $sql . ' AND Ten_tai_san LIKE %' . $Ten . '%';
+        }
+        if ($Type != null) {
+            $sql = $sql . ' AND Type = ' . $Type;
+        }
+        if ($Status != null) {
+            $sql = $sql . ' AND Status = ' . $Status;
+        }
+        if ($Place != null) {
+            $sql = $sql . ' AND Place LIKE %' . $Place . '%';
+        }
+        if ($StartPrice != null) {
+            $sql = $sql . ' AND Price >= ' . $this->_db->quote($StartPrice, 'INTEGER');
+        }
+        if ($EndPrice != null) {
+            $sql = $sql . ' AND Price <= ' . $this->_db->quote($EndPrice, 'INTEGER');
+        }
+        if ($StartTime != null) {
+            $sql = $sql . ' AND WarrantyTime >= ' . $this->_db->quote($StartTime, 'INTEGER');
+        }
+        if ($EndTime != null) {
+            $sql = $sql . ' AND WarrantyTime <= ' . $this->_db->quote($EndTime, 'INTEGER');
+        }
+        if ($StartDate != null) {
+            $sql = $sql . ' AND StartDate >= ' . $StartDate;
+        }
+        if ($EndDate != null) {
+            $sql = $sql . ' AND StartDate <= ' . $EndDate;
+        }
+        $sql = $sql . ';';
+        $result = $this->fetchAll($this->select()->where($sql));
+        if (!$result) {
+            return null;
+        }
+        return $result;
+    }
+
+    /**
+     * function checkMaTS()
+     * 
+     * @todo check exist Ma_tai_san
+     * @param $MaTS int
+     * @return NULL if not exist
+     * @return ItemID if exist
+     */
+    private function checkMaTS($MaTS) {
+        $MaTS = (int) $MaTS;
+        $row = $this->fetchRow("Ma_tai_san = '$MaTS'");
+        if ($row == NULL) {
+            return NULL;
+        }
+        return $row->ItemID;
+    }
+
+}
+
